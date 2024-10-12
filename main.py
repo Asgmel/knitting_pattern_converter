5f9eab42b9f8ff52d7cfcad56937b619d9aab89c
Asgeir Melling <asgmel@gmail.com>
2024-10-12 23:05:19 +0200

    feat: add sv_ttk theme dark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import sv_ttk
+
 root = Tk()
 root.title("Marits Knitting Tool")
 root.geometry("500x500")
@@ -7,4 +9,6 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+sv_ttk.set_theme("dark")
 root.mainloop()
